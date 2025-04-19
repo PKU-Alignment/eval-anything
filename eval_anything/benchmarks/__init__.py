f85587d8ddcd3144a3b237806331c255a41f143a
Ethyn13 <yuhaozh13@outlook.com>
2025-04-19 10:39:08 -0700

    feat: update spoc evaluation

---
+++ eval_anything/benchmarks/__init__.py
@@ -0,0 +1,21 @@
+import importlib
+
+benchmark_modules = [
+    "benchmarks.text_to_text.gsm8k.eval",
+    "benchmarks.text_to_text.mmlu.eval",
+    "benchmarks.text_to_text.HumanEval.eval",
+    "benchmarks.text_to_text.AGIEval.eval",
+    "benchmarks.text_to_text.TruthfulQA.eval",
+    "benchmarks.text_to_text.ARC.eval",
+    "benchmarks.text_to_text.CMMLU.eval",
+    "benchmarks.text_to_text.MMLUPRO.eval",
+    "benchmarks.text_to_text.CEval.eval",
+    "benchmarks.text_image_to_text.mmmu.eval",
+    "benchmarks.text_image_to_text.mathvision.eval",
+    "benchmarks.text_audio_to_text.mmau.eval",
+    "benchmarks.text_video_to_text.mmvu.eval",
+    "benchmarks.text_vision_to_action.chores.eval",
+]
+
+for module in benchmark_modules:
+    importlib.import_module(module)
